73f69683660d2b80a3eb43a49d3a5dbf4fe4f32a
Shashwat Pandey <shashwatpandey970@gmail.com>
2025-05-07 02:32:08 +0530

    commit: inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+interface TechStack {
+  name: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio';
+  isDarkTheme = false;
+
+  techStack: TechStack[] = [
+    { name: 'Angular', icon: 'assets/tech/angular.svg' },
+    { name: 'TypeScript', icon: 'assets/tech/typescript.svg' },
+    { name: 'Node.js', icon: 'assets/tech/nodejs.svg' },
+    { name: 'Spring', icon: 'assets/tech/spring.svg' }
+  ];
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+    document.body.classList.toggle('dark-theme');
+  }
 }
